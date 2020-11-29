4c97b32ebf30d4e97bc461ed783baf5a4ca4e1a7
jasonxsiervo <43787504+jasonxsiervo@users.noreply.github.com>
2020-11-30 05:16:46 +0800

    Includes breaking functionalities and unused pckgs

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,12 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import { addTask } from './actions';
 import Textfields from './components/Textfields';
 import Calendar from './components/Calendar';
 import SingleTask from './components/SingleTask';
 
-function Home() {
+function Home({ logout, ...rest }) {
 
     var blankTaskState = {
         id: undefined,
@@ -23,25 +24,25 @@ function Home() {
     // console.log("tasks sa home: ", tasks);
 
     useEffect(() => {
-        console.log('task updated!', task)
-    }, [tasks]);
+        // console.log('task updated!', task)
+    }, [task]);
 
     const submitForm = async (event) => {
         event.preventDefault();
-        console.log("title: ", textData.data.title);
-        console.log("details: ", textData.data.details);
-        console.log("id: ", textData.id);
+        console.log("title: ", textData.data);
+        // console.log("details: ", textData.data.details);
+        // console.log("id: ", textData.id);
         blankTaskState = await {
-            id: textData.id,
-            title: textData.data.title,
-            details: textData.data.details,
-            date: date,
-            time: time,
+            id: textData.id || '',
+            title: textData.data.title || '',
+            details: textData.data.details || '',
+            date: date || '',
+            time: time || '',
         };
         console.log(blankTaskState);
         // await setTask({ ...blankTaskState });
         dispatch(addTask(blankTaskState));
-    }
+    };
 
     return (
         <div className="Home">
@@ -51,7 +52,11 @@ function Home() {
                         <ul className="tasks-list">
                             {tasks.map(task => {
                                 return (
-                                    <SingleTask key={task.id} task={task} />
+                                    <SingleTask
+                                        key={task.id}
+                                        task={task}
+                                        getData={(data) => setTask(data)}
+                                    />
                                 )
                             })}
 
@@ -61,13 +66,13 @@ function Home() {
                 <form onSubmit={submitForm}>
                     <div className="task-view">
                         <div className="task-textfields">
-                            <Textfields passData={(data) => setTextData(data)} />
+                            <Textfields task={task} passData={(data) => setTextData(data)} />
                         </div>
                         <div className="task-calendar">
-                            <Calendar passDate={(date) => setDate(date)} passTime={(time) => setTime(time)} />
+                            <Calendar task={task} passDate={(date) => setDate(date)} passTime={(time) => setTime(time)} />
                         </div>
                         <div className="submit-area">
-                            <button className="submit-button" type="submit">
+                            <button title="Submit" className="submit-button" type="submit">
                                 +
                             </button>
                         </div>
@@ -78,4 +83,4 @@ function Home() {
     );
 }
 
-export default Home;
+export default withRouter(Home);
--- src/components/Textfields.js
+++ src/components/Textfields.js
@@ -1,49 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import '../styles/Textfields.css';
 
 
-function Textfields({ passData }) {
+function Textfields(props) {
 
     const [id, setId] = useState('');
     const [data, setData] = useState({
         title: '',
         details: ''
     });
+    const { task } = props.task;
+
+
+    // useEffect(() => {
+    //     console.log('THERE IS AN UPDATE IN TEXTFIELD', props.task.task.id);
+    //     // console.log("props.task.title: ", props.task.title);
+    //     // console.log("props.task.details: ", props.task.title);
+    //     setData({ title: props.task.task.title, details: props.task.task.details });
+    //     console.log('value of component state: ', data);
+    // }, [props.task])
 
     function handleChange(e) {
-        // switch (e.target.className) {
-        //     case 'title':
-        //         setTitle(e.target.value);
-        //         // passData({ title,  });
-        //         break;
-        //     case 'details':
-        //         setDetails(e.target.value);
-        //         // passData({ details });
-        //         break;
-        //     default:
-        //         console.log("none");
-        //         break;
-        // }
         setData(prev => ({ ...prev, [e.target.className]: e.target.value }));
         setId(uuidv4());
-        passData({ id, data });
-        // console.log(title + ' space ' + details);
+        props.passData({ id, data });
     }
 
     return (
         <div className="title-details-div">
             <input
                 type="text"
+                //defaultValue={data.title}
                 className="title"
-                placeholder="Title"
+                placeholder={task ? task.title : "Title"}
                 onChange={(e) => handleChange(e)}
             />
             <textarea
                 type="text"
+                //defaultValue={data.details}
                 className="details"
                 rows="27"
-                placeholder="Details"
+                placeholder={task ? task.details : "Details"}
                 onChange={(e) => handleChange(e)}
             />
         </div>
