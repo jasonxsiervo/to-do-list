4e9b111533d4e421d6a0c95a5bf1861559089d91
jasonxsiervo <43787504+jasonxsiervo@users.noreply.github.com>
2020-11-30 05:11:23 +0800

    Added strike and delete functionality

--- src/components/SingleTask.js
+++ src/components/SingleTask.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { deleteTask } from '../actions';
+import { useDispatch } from 'react-redux';
 
 const SingleTask = (props) => {
-    // console.log("props: ", props)
-    return (
-        <li className="single-task">
-            <span className="emoji">😀</span>
-            <div className="single-task-details">
-                <span className="title-tag">{props.task.title}</span>
-                <span className="description-tag">{props.task.details}</span>
-            </div>
 
+    const [strike, setStrike] = useState(false);
+    // console.log("props: ", props.task)
+    const dispatch = useDispatch();
+    const { task } = props;
+
+    const handleDelete = () => {
+        // console.log('delete!');
+        // console.log("id: ", props.task.id);
+        dispatch(deleteTask(props.task.id));
+    }
+
+    const handleClick = () => {
+        props.getData({ task });
+    }
+
+    const strikeText = () => {
+        setStrike(!strike);
+    }
+
+    return (
+        <li className="single-task" onClick={handleClick}>
+            <span className="emoji">🐧</span>
+            <div className="single-task-details">
+                <span className={strike ? "title-tag-strike" : "title-tag"}>{task.title}</span>
+                <span className={strike ? "description-tag-strike" : "description-tag"}>{task.details}</span>
+            </div>
+            {!strike ? <button title="Done" className="done-button" onClick={strikeText}>✅</button> : <butto className="done-button" onClick={strikeText} disabled></butto>}
+            <button title="Delete" className="delete-button" onClick={handleDelete}>❌</button>
         </li>
     )
 };
