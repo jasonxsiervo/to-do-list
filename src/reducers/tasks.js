1be36e1bd28bcb644f9fc762856152ef98a13a0c
jasonxsiervo <43787504+jasonxsiervo@users.noreply.github.com>
2020-11-30 05:09:15 +0800

    Working delete functionality

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -6,7 +6,7 @@ const dataTasks = [
         "details": "Get apples",
         "date": "Mon Nov 02 2020 12:00:00 GMT+0800 (Philippine Standard Time) {}",
         "time": `23:11`,
-    },
+    }
 ];
 
 const tasksReducer = (state = dataTasks, action) => {
@@ -20,6 +20,14 @@ const tasksReducer = (state = dataTasks, action) => {
             return [...state, { ...action.payload }];
         case 'EDIT_TASK':
             return state - 1;
+        case 'VIEW_TASK':
+            return state - 1;
+        case 'DELETE_TASK':
+            const copyState = [...state];
+            console.log("ID: ", action.payload);
+            const i = copyState.findIndex(x => x.id === action.payload);
+            copyState.splice(i, 1);
+            return [...copyState];
         default:
             return state;
     }
